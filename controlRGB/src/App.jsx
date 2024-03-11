8d9ae6f1dfd07cdceca3eb548c001f5f99a8e458
sga1d0s <galdos.sergio@outlook.com>
2024-03-11 21:53:19 +0100

    prueba sin valor inicial

--- controlRGB/src/App.jsx
+++ controlRGB/src/App.jsx
@@ -6,37 +6,42 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            valueRed: 0,
+            // valueRed: 0,
+            
             valueBlue: 0,
             valueGreen: 0,
         }
     }
 
-    handleSlide(value) {
-        console.log(value)
-    }
+    // handleSlide(value) {
+    //     console.log(value)
+    // }
+
+    handleSliderChange = (event) => {
+        console.log({ valueRed: event.target.value })
+      };
 
     render() {
         return (
             <>
                 <h1>Control RGBA</h1>
- 
+
                 <div className='frame'>
 
                 </div>
                 <div className='sliders'>
 
                     <Slider
-                        defaultValue={this.state.valueRed}
+                        // defaultValue={this.state.valueRed}
+                        defaultValue={0}
                         aria-label="Red"
                         valueLabelDisplay="auto"
-                        onChange={() => { this.handleSlide(event.target.value) }} />
-
+                        onChange={this.handleSliderChange}
+                    />
 
                     <Slider defaultValue={this.state.valueBlue} aria-label="Blue" valueLabelDisplay="auto" />
                     <Slider defaultValue={this.state.valueGreen} aria-label="Green" valueLabelDisplay="auto" />
-                    <Slider
-                        onChange={() => { console.log(this.state.value) }} />
+
                 </div>
             </>
         );

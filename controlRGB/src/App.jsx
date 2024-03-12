3551916145f4c99c48cd5a2253e81a9a4edda74d
we1ngr0 <40735655+sga1d0s@users.noreply.github.com>
2024-03-12 11:55:13 +0100

    rojo funciona

--- controlRGB/src/App.jsx
+++ controlRGB/src/App.jsx
@@ -9,11 +9,14 @@ class App extends Component {
             valueRed: 0,
             valueBlue: 0,
             valueGreen: 0,
+            index: 0,
         }
     }
 
     handleSliderChangeRed = (event) => {
         console.log({ valueRed: event.target.value })
+        this.setState({ valueRed: event.target.value })
+
     };
 
     handleSliderChangeBlue = (event) => {
@@ -28,28 +31,34 @@ class App extends Component {
             <>
                 <h1>Control RGBA</h1>
 
-                <div className='frame'>
-
+                <div className='frame' style={{ backgroundColor: `rgb(${this.state.valueRed}, ${this.state.valueGreen}, ${this.state.valueBlue})` }}>
+                    {/* Contenido de la clase .frame */}
                 </div>
+
                 <div className='sliders'>
 
                     <Slider
+                        id="red"
                         defaultValue={this.state.valueRed}
                         aria-label="red"
                         valueLabelDisplay="auto"
                         onChange={this.handleSliderChangeRed}
+                        key={0}
+                        value={this.state.red}
                     />
                     <Slider
                         defaultValue={this.state.valueBlue}
                         aria-label="blue"
                         valueLabelDisplay="auto"
                         onChange={this.handleSliderChangeBlue}
+                        key={1}
                     />
                     <Slider
                         defaultValue={this.state.valueGreen}
                         aria-label="green"
                         valueLabelDisplay="auto"
                         onChange={this.handleSliderChangeGreen}
+                        key={2}
                     />
 
 

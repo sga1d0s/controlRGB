b15df2894e3ded4102051e14f91be417dc3cde9e
sga1d0s <galdos.sergio@outlook.com>
2024-03-12 23:29:08 +0100

    colores ok

--- controlRGB/src/App.jsx
+++ controlRGB/src/App.jsx
@@ -9,11 +9,12 @@ class App extends Component {
             valueRed: 0,
             valueBlue: 0,
             valueGreen: 0,
+            opacity: 100
         }
     }
 
     // Cambio UNICO
-    handleSliderChangeColor = (event) => {
+    handleSliderChange = (event) => {
         if (event.target.name == "red") {
             this.setState({ valueRed: event.target.value })
         }
@@ -22,6 +23,9 @@ class App extends Component {
         }
         if (event.target.name == "green") {
             this.setState({ valueGreen: event.target.value })
+        }
+        if (event.target.name == "opacity") {
+            this.setState({ opacity: event.target.value })
         }
     };
 
@@ -32,41 +36,62 @@ class App extends Component {
 
                 <div className='frame'
                     // Cambio de estilo literales `${valor}`
-                    style={{ backgroundColor: `rgb(${this.state.valueRed}, ${this.state.valueGreen}, ${this.state.valueBlue})` }}>
+                    style={{ backgroundColor: `rgba(${this.state.valueRed}, ${this.state.valueGreen}, ${this.state.valueBlue}, ${this.state.opacity / 100})`,
+                    boxShadow: `10px 10px 10px rgba(${this.state.valueRed}, ${this.state.valueGreen}, ${this.state.valueBlue}, ${this.state.opacity / 190})` }}>
                 </div>
 
                 <div className='sliders'>
 
-                    <label htmlFor="">RED</label>
-                    <Slider
-                        // defaultValue={this.state.valueBlue}
-                        value={this.state.valueRed}
-                        onChange={this.handleSliderChangeColor}
-                        name="red"
-                        min={0}
-                        max={255}
-                        valueLabelDisplay='on'
-                    />
-                    <label htmlFor="">BLUE</label>
-                    <Slider
-                        value={this.state.valueBlue}
-                        onChange={this.handleSliderChangeColor}
-                        name="blue"
-                        min={0}
-                        max={255}
-                        valueLabelDisplay='on'
-                    />
-                    <label htmlFor="">GREEN</label>
-                    <Slider
-                        value={this.state.valueGreen}
-                        onChange={this.handleSliderChangeColor}
-                        name="green"
-                        min={0}
-                        max={255}
-                        valueLabelDisplay='on'
-                    />
+                    <div className='contSlider'>
+                        <label style={{color:`red`}}>RED</label>
+                        <Slider
+                            // defaultValue={this.state.valueBlue}
+                            value={this.state.valueRed}
+                            onChange={this.handleSliderChange}
+                            name="red"
+                            min={0}
+                            max={255}
+                            valueLabelDisplay='on'
+                        />
+                    </div>
 
+                    <div className='contSlider'>
+                        <label style={{color: `blue`}}>BLUE</label>
+                        <Slider
+                            // defaultValue={this.state.valueBlue}
+                            value={this.state.valueBlue}
+                            onChange={this.handleSliderChange}
+                            name="blue"
+                            min={0}
+                            max={255}
+                            valueLabelDisplay='on'
+                        />
+                    </div>
 
+                    <div className='contSlider'>
+                        <label style={{color: `green`}}>GREEN</label>
+                        <Slider
+                            // defaultValue={this.state.valueBlue}
+                            value={this.state.valueGreen}
+                            onChange={this.handleSliderChange}
+                            name="green"
+                            min={0}
+                            max={255}
+                            valueLabelDisplay='on'
+                        />
+                    </div>
+                    <div className='contSlider'>
+                        <label htmlFor="">OPACITY</label>
+                        <Slider
+                            // defaultValue={this.state.valueBlue}
+                            value={this.state.opacity}
+                            onChange={this.handleSliderChange}
+                            name="opacity"
+                            min={0}
+                            max={100}
+                            valueLabelDisplay='on'
+                        />
+                    </div>
                 </div>
             </>
         );
